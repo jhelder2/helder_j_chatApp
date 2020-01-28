a0ad2f24cfb350764cf9a10e0e611382c702927a
Jeffrey Helder <jeffrey.d.helder@gmail.com>
2020-01-28 16:29:06 -0500

    basic set up of serverside and userside

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,3 +1,15 @@
 // imports always go first - if we're importing anything
 
 const socket = io();
+
+function setUserId(packet){
+    console.log(packet);
+};
+
+function runDisconnectMessage(packet) {
+    console.log(packet);
+};
+
+// some event handling -> these events are coming from the server //
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('user_disconnect', runDisconnectMessage);
