959c915123146f9f16d873beabec40215dd6820f
Jeffrey Helder <jeffrey.d.helder@gmail.com>
2020-01-28 15:44:22 -0500

    attached socket.io

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+const io = require('socket.io')();
+// ^^ instantiate the socket.io Library right away with the () method --> makes it run //
+
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -12,4 +15,18 @@ app.get('/', (req, res) => {
 
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
+});
+
+// this is all our socket.io messaging functionality //
+
+// attach socket.io //
+io.attach(server);
+
+io.on('connection', function(socket) {
+    console.log('user connected');
+
+// listen for disconnect event //
+    socket.on('disconnect', function() {
+        console.log('a user disconnected');
+    })
 });
